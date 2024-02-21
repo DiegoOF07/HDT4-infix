7a698a68ee1498556efea10b7fbb7ba5eb825ab4
Diego Flores <floresdiego041@gmail.com>
2024-02-20 21:23:52 -0600

    Creating IStack interface

---
+++ IStack.java
@@ -0,0 +1,19 @@
+/**
+ * @author Diego Flores y Juan Solís
+ * @creationDate 20/02/2024
+ * @description Interfaz que define los métodos para el funcionamiento de la pila
+ */
+
+ public interface IStack<T> {
+    /**
+    * @description Agrega un elemento a la parte superior de la pila
+    * @param value
+    */
+    public void push(T value);
+
+    /**
+    * @description Elimina y retorna el elemento que se encuentra en la parte superior de la pila
+    * @return T
+    */
+    public T pop();
+}
